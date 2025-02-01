11edf02a0552dab639aaeee6014c1aeeba75ff23
Joseolezi <jfan2@hotmail.com>
2025-02-01 19:07:15 +0100

    update

--- main.js
+++ main.js
@@ -13,64 +13,16 @@ function closepop() {
     popupOverlay.style.display="none";
 }
 
+  
+function openform () {
 
-const InputName = document.getElementById("InputName")
-const InputEmail = document.getElementById("InputEmail")
-const InputTel = document.getElementById("InputTel")
-
-const mensaje = document.getElementById("mensaje");
-
-function sendEmail() {
-    const name = InputName.value;
-    const email = InputEmail.value;
-    const phone = InputTel.value;
-    const subject = motivo.value;
-    const message = mensaje.value;
-
-    const emailBody = `
-        Name: ${name}
-        Email: ${email}
-        Phone: ${phone}
-        Subject: ${subject}
-        Message: ${message}
-    `;
-
-    window.location.href = `mailto:jose.fandos.pgs@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(emailBody)}`;
+    document.getElementById("formpop")
+    formpop.style.display="block";
 }
 
-  const constraints = {
-      name: {
-          presence: { allowEmpty: false }
-      },
-      email: {
-          presence: { allowEmpty: false },
-          email: true
-      },
-      message: {
-          presence: { allowEmpty: false }
-      }
-  };
+function closeform () {
 
-  const form = document.getElementById('formulario');
+    document.getElementById("formpop")
+    formpop.style.display="none";
 
-  form.addEventListener('submit', function (event) {
-    const formValues = {
-        name: form.elements.name.value,
-        email: form.elements.email.value,
-        message: form.elements.message.value
-    };
-
-    const errors = validate(formValues, constraints);
-
-    if (errors) {
-      event.preventDefault();
-      const errorMessage = Object
-          .values(errors)
-          .map(function (fieldValues) { return fieldValues.join(', ')})
-          .join("\n");
-
-      alert(errorMessage);
-    }
-  }, false);
-
-
+}
