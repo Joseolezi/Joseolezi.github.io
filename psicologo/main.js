76542ed0d492ee8a34f3833a00e225bd643bf4bb
Joseolezi <jfan2@hotmail.com>
2025-02-07 20:42:05 +0100

    update

--- psicologo/main.js
+++ psicologo/main.js
@@ -131,21 +131,3 @@ function toggleTheme() {
 
 }
 
-(function() {
-    const buttons = document.querySelectorAll(".btn-posnawr");
-  
-    buttons.forEach(button => {
-      ["mouseenter", "mouseout"].forEach(evt => {
-        button.addEventListener(evt, e => {
-          let parentOffset = button.getBoundingClientRect(),
-              relX = e.pageX - parentOffset.left,
-              relY = e.pageY - parentOffset.top;
-  
-          const span = button.getElementsByTagName("span");
-  
-          span[0].style.top = relY + "px";
-          span[0].style.left = relX + "px";
-        });
-      });
-    });
-  })();
